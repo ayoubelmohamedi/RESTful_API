aad50cfe4d1f1f8beaf9f1a6f49aab678e37e26a
ayoubelmohamedi <ayoubprog61@gmail.com>
2023-06-02 22:32:50 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
+import cors from 'cors';
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
 
 const app: Application = express();
+
+// if you want anyone to be able to connect
+//app.use(cors({ origin: true }))
+app.use(cors({ origin: "<http://localhost:3000>" }))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
